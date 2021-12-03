982644333e5fceb1dd45169e2aa88326e5957f7b
Federico <federico_mameli@yahoo.com>
2021-12-03 11:48:19 +0100

    feat: add Vue recaptcha plugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
 import vueCompositionApi from "@vue/composition-api";
 import Vuelidate from "vuelidate";
+import { VueReCaptcha } from "vue-recaptcha-v3";
 
 //add plugins
 Vue.use(vueCompositionApi);
 Vue.use(Vuelidate);
+Vue.use(VueReCaptcha, {
+  siteKey: import.meta.env.VITE_RECAPTCHA_PUBLIC_KEY,
+  loaderOptions: {
+    useEnterprise: true,
+  },
+});

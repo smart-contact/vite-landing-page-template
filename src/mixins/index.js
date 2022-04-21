5ea26a84cfa45b2f0062c1d9664a46ef2cd8cd41
Alessandro Pintus <alessandro.pintus94@gmail.com>
2022-04-21 15:30:58 +0200

    fix: importazioni mixins

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,3 +1,1 @@
-export { default as vuelidateGetStateMixin } from "./vuelidateGetStateMixin";
-export { default as privaciesFormSchemasMixin } from "./privaciesFormSchemasMixin";
 export { default as getImgURLMixin } from "./getImgURL";

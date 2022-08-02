74d2fa17e188ba6d796e1590256450f5b643328d
Alessandro Pintus <alessandro.pintus94@gmail.com>
2022-08-02 15:03:35 +0200

    fix: eliminato recaptcha

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,12 +1,5 @@
 import Vue from "vue";
 import Vuelidate from "vuelidate";
-import { VueReCaptcha } from "vue-recaptcha-v3";
 
 //add plugins
 Vue.use(Vuelidate);
-Vue.use(VueReCaptcha, {
-  siteKey: import.meta.env.VITE_RECAPTCHA_PUBLIC_KEY,
-  loaderOptions: {
-    useEnterprise: true,
-  },
-});

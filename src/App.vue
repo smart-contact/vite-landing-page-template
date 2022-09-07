f734894db2884ec3428d1f6212eafe9b6c566a68
Alessandro Pintus <alessandro.pintus94@gmail.com>
2022-09-07 17:38:27 +0200

    feat: modifiche per vue 2.7 di eslint

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,8 @@
   </div>
 </template>
 
-<script>
-import { computed, defineComponent, onBeforeMount, inject } from "vue";
+<script setup>
+import { computed, provide, onBeforeMount, inject } from "vue";
 import { breakpointsBootstrapV5, useBreakpoints } from "@vueuse/core";
 import {
   useProducts,
@@ -48,119 +48,90 @@ import {
 import OverlayLoadingScreen from "@/components/OverlayLoadingScreen.vue";
 import MqFooter from "@/components/MqFooter.vue";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    SHeaderBrands,
-    // SCallMeBackForm,
-    MqFooter,
-    SCallMeBackModal,
-    OverlayLoadingScreen,
-  },
-  provide() {
-    return {
-      isMobile: this.breakpoints.smaller("md"),
-      isTablet: this.breakpoints.greater("sm"),
-      isDesk: this.breakpoints.greater("md"),
-      params: this.landing.params.get(),
-      onProductSelected: this.onProductSelected,
-      sendLead: this.sendLead,
-    };
-  },
-
-  setup() {
-    const $bvModal = inject("$bvModal");
-    const callMeBackFormOptions = inject("callMeBackFormOptions");
-    const landing = useLanding();
-    const { logoAccountMobile, logoAccount, account } = landing.params.get();
-    const products = useProducts();
-    const lead = useLead({
-      disableRecaptchaCheck: !landing.params.get("useRecaptcha"),
-    });
-
-    const breakpoints = useBreakpoints(breakpointsBootstrapV5);
-    const accountLogos = [
-      {
-        src: `${LIVELANDING_CDN_IMAGES_BASE_URL}/${logoAccount}`,
-        media: "(min-width: 768px)",
-      },
-      {
-        src: `${LIVELANDING_CDN_IMAGES_BASE_URL}/${logoAccountMobile}`,
-        alt: `logo ${account}`,
-        default: true,
-        media: "(max-width: 767.98px)",
-      },
-    ];
-
-    const buyerLogo = computed(() => {
-      const [buyer] = products.buyers.value;
-      return buyer
-        ? {
-            src: buyer.imageUrl,
-            alt: `logo ${buyer.name}`,
-          }
-        : {};
-    });
-
-    const sendLead = async (data = {}) => {
-      const { successURL } = landing.params.get();
-      try {
-        if (window.dataLayer) {
-          window.dataLayer.push({
-            event: "form_submit",
-            eventCategory: "form",
-            eventAction: "submit_ok",
-            eventLabel: "call-back landing",
-          });
-        }
-        await lead.send(data);
-        if (successURL) {
-          window.location.href = successURL;
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    const onProductSelected = (productIndex) => {
-      products.setSelectedIndex(productIndex);
-      $bvModal.show("call-me-back-modal");
-    };
-
-    const modalEvents = {
-      show: () => {
-        if (products.selected.value != undefined) {
-          landing.data.set("buyer", products.selected.value.buyer.name);
-          landing.data.set("offer", products.selected.value.name);
-        }
-      },
-      hide: () => {
-        landing.data.restoreDefaults();
-        products.setSelectedIndex(undefined);
-      },
-    };
-
-    onBeforeMount(() => {
-      products.load({
-        // collection: landing.params.get('collection'),
-        productsIds: landing.params.get("products"),
-      });
-    });
-
-    return {
-      breakpoints,
-      products,
-      accountLogos,
-      buyerLogo,
-      lead,
-      landing,
-      sendLead,
-      onProductSelected,
-      callMeBackFormOptions,
-      modalEvents,
-    };
-  },
+const $bvModal = inject("$bvModal");
+const callMeBackFormOptions = inject("callMeBackFormOptions");
+const landing = useLanding();
+const { logoAccountMobile, logoAccount, account } = landing.params.get();
+const products = useProducts();
+const lead = useLead({
+  disableRecaptchaCheck: !landing.params.get("useRecaptcha"),
 });
+
+const breakpoints = useBreakpoints(breakpointsBootstrapV5);
+const accountLogos = [
+  {
+    src: `${LIVELANDING_CDN_IMAGES_BASE_URL}/${logoAccount}`,
+    media: "(min-width: 768px)",
+  },
+  {
+    src: `${LIVELANDING_CDN_IMAGES_BASE_URL}/${logoAccountMobile}`,
+    alt: `logo ${account}`,
+    default: true,
+    media: "(max-width: 767.98px)",
+  },
+];
+
+const buyerLogo = computed(() => {
+  const [buyer] = products.buyers.value;
+  return buyer
+    ? {
+        src: buyer.imageUrl,
+        alt: `logo ${buyer.name}`,
+      }
+    : {};
+});
+
+const sendLead = async (data = {}) => {
+  const { successURL } = landing.params.get();
+  try {
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        event: "form_submit",
+        eventCategory: "form",
+        eventAction: "submit_ok",
+        eventLabel: "call-back landing",
+      });
+    }
+    await lead.send(data);
+    if (successURL) {
+      window.location.href = successURL;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const onProductSelected = (productIndex) => {
+  products.setSelectedIndex(productIndex);
+  $bvModal.show("call-me-back-modal");
+};
+
+const modalEvents = {
+  show: () => {
+    if (products.selected.value != undefined) {
+      landing.data.set("buyer", products.selected.value.buyer.name);
+      landing.data.set("offer", products.selected.value.name);
+    }
+  },
+  hide: () => {
+    landing.data.restoreDefaults();
+    products.setSelectedIndex(undefined);
+  },
+};
+
+onBeforeMount(() => {
+  products.load({
+    // collection: landing.params.get('collection'),
+    productsIds: landing.params.get("products"),
+  });
+});
+
+provide("isMobile", breakpoints.smaller("md"));
+provide("isTablet", breakpoints.greater("sm"));
+provide("isDesk", breakpoints.greater("md"));
+provide("params", landing.params.get());
+provide("onProductSelected", onProductSelected);
+provide("sendLead", sendLead);
 </script>
 
 <style lang="scss">
@@ -172,7 +143,7 @@ export default defineComponent({
       width: 100%;
     }
   }
-   input::placeholder {
+  input::placeholder {
     font-size: 0.75em;
   }
 }
